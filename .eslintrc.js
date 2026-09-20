f54ddd59cb62a31cb2dea5fa0e74015e7f603e7c
wassim askri <wissask1234@gmail.com>
2026-09-20 13:08:19 +0100

    Add Playwright project configuration and scaffolding
    Sets up ESLint, Prettier, TypeScript, and Playwright config along with
    base folder structure for tests, pages, and fixtures.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'playwright'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:playwright/recommended',
+    'prettier',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    // We assert on API responses inside loops/branches for CRUD flows.
+    'playwright/no-conditional-in-test': 'off',
+  },
+  ignorePatterns: ['node_modules', 'playwright-report', 'test-results', 'dist'],
+};
